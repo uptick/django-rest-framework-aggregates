e68df9b993ab1a05ed60146c975b220605648a58
Fiona Lawrence <fiona.lawrence@aba-systems.com.au>
2017-12-15 10:22:58 +1100

    Update http status codes

--- drf_aggregates/exceptions.py
+++ drf_aggregates/exceptions.py
@@ -4,11 +4,11 @@ __all__ = ['AggregateException', 'QueryException']
 
 
 class AggregateException(APIException):
-    status_code = 400
+    status_code = 422
     default_detail = 'An error has occured in the aggregate API.'
     default_code = 'aggregate_error'
 
 
 class QueryException(AggregateException):
-    status_code = 403
+    status_code = 400
     default_detail = 'Incorrect query parameters.'
